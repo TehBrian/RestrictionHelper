17f52bb73990555495949ca23287a17b731a831a
TehBrian <tehbrian@outlook.com>
2021-04-06 23:09:14 -0400

    build: Move common build code to buildSrc plugin

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -1,14 +1,8 @@
 plugins {
-    id("java")
-    id("maven")
-    id("org.checkerframework") version "0.5.19"
+    id("restrictionhelper.java-conventions")
 }
 
-group = "xyz.tehbrian.restrictionhelper"
-version = "0.2.0"
-
 repositories {
-    mavenCentral()
     mavenLocal() // for testing
 
     maven {
@@ -22,12 +16,6 @@ repositories {
 }
 
 dependencies {
-    // JUnit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
-
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
-
     implementation(project(":core"))
 
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
@@ -36,15 +24,4 @@ dependencies {
     compileOnly("com.plotsquared:PlotSquared-Core:5.13.3") // PlotSquared Core API
     compileOnly("com.plotsquared:PlotSquared-Bukkit:5.13.3") // PlotSquared Bukkit API
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.4") // WorldGuard API
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-    }
 }
