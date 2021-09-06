958d094cc7950959dd3783dc519b26c220608361
TehBrian <tehbrian@protonmail.com>
2021-09-05 21:52:44 -0400

    build: use group, version, description from root project

--- buildSrc/src/main/kotlin/restrictionhelper.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/restrictionhelper.java-conventions.gradle.kts
@@ -6,9 +6,9 @@ plugins {
     id("net.kyori.indra.publishing")
 }
 
-group = "xyz.tehbrian.restrictionhelper"
-version = "0.2.0-SNAPSHOT"
-description = "A small shade-in dependency for plugin developers, to ease the pain of checking the building restrictions of various plugins."
+group = rootProject.group
+version = rootProject.version
+description = rootProject.description
 
 repositories {
     mavenCentral()
