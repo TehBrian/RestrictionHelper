6f085501c7f2ad06e43487f7c78abaccaf98053b
TehBrian <tehbrian@protonmail.com>
2021-10-23 15:50:23 -0400

    version 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "xyz.tehbrian.restrictionhelper"
-version = "0.2.0-SNAPSHOT"
+version = "0.2.0"
 description = "A small shade-in dependency for plugin developers, to ease the pain of checking the building restrictions of various plugins."
 
 indraSonatype {
