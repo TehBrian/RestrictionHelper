a8649996c25804f6befb5ec4c9ffb3a7017acab4
TehBrian <tehbrian@proton.me>
2023-03-10 21:01:05 -0500

    version 0.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 group = "dev.tehbrian.restrictionhelper"
-version = "0.3.2"
+version = "0.3.3"
 description = "A small library that eases the pain of checking the build restrictions of various plugins."
