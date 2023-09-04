0258d5361cd40ba92a44065c279aac6162281173
TehBrian <tehbrian@proton.me>
2023-09-04 11:33:44 -0400

    mc 1.20.1; update restriction deps

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -4,19 +4,16 @@ plugins {
 
 repositories {
   mavenCentral()
-  maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-
-  // Built-in Restrictions
-  maven("https://maven.enginehub.org/repo/") // WorldGuard
+  maven("https://repo.papermc.io/repository/maven-public/")
+  maven("https://maven.enginehub.org/repo/")
 }
 
 dependencies {
   api(project(":restrictionhelper-core"))
+  compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
 
-  compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
-
-  // Built-in Restrictions
-  compileOnly("com.plotsquared:PlotSquared-Core:6.11.1") { isTransitive = false }
-  compileOnly("com.plotsquared:PlotSquared-Bukkit:6.11.1") { isTransitive = false }
-  compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.7")
+  implementation(platform("com.intellectualsites.bom:bom-newest:1.37"))
+  compileOnly("com.intellectualsites.plotsquared:plotsquared-core:7.0.0")
+  compileOnly("com.intellectualsites.plotsquared:plotsquared-bukkit:7.0.0") { isTransitive = false }
+  compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.9")
 }
