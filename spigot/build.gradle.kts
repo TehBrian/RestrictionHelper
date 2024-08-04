7f018f305ec5cebdd91181fc7d6bb0c968731c41
TehBrian <tehbrian@proton.me>
2024-08-04 15:13:38 -0400

    update deps

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -10,10 +10,10 @@ repositories {
 
 dependencies {
   api(project(":restrictionhelper-core"))
-  compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+  compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
 
-  implementation(platform("com.intellectualsites.bom:bom-newest:1.37"))
-  compileOnly("com.intellectualsites.plotsquared:plotsquared-core:7.0.0")
-  compileOnly("com.intellectualsites.plotsquared:plotsquared-bukkit:7.0.0") { isTransitive = false }
-  compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.9")
+  implementation(platform("com.intellectualsites.bom:bom-newest:1.47"))
+  compileOnly("com.intellectualsites.plotsquared:plotsquared-core:7.38")
+  compileOnly("com.intellectualsites.plotsquared:plotsquared-bukkit:7.38") { isTransitive = false }
+  compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.10")
 }
