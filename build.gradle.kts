f5d68e1f07ccc81fa0298a5f8d19d4732a9c384e
TehBrian <tehbrian@proton.me>
2024-08-14 12:31:08 -0400

    change group to dev.tehbrian

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@ plugins {
 	id("com.github.ben-manes.versions") version "0.51.0"
 }
 
-group = "dev.tehbrian.restrictionhelper"
+group = "dev.tehbrian"
 version = "0.4.1"
 description = "A small library that eases the pain of checking the build restrictions of various plugins."
